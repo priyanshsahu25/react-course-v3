776091e4b830b8a846d9c53b3964b42d78fde1f9
Priyansh Sahu <priyansh.sahu25@gmail.com>
2025-02-11 10:28:13 +0000

    Commit

--- 04-fundamental-projects/06-tabs/starter/src/App.jsx
+++ 04-fundamental-projects/06-tabs/starter/src/App.jsx
@@ -1,6 +1,48 @@
+import { useEffect, useState } from "react";
+
 const url = 'https://www.course-api.com/react-tabs-project';
 
+
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [data,setData]=useState([])
+  const [titles,setTitles]=useState([])
+  const [showTitle,setTitle]=useState({})
+
+  useEffect(()=>{
+    async function fetchData (){
+      const res= await fetch(url)
+      const newData=await res.json()
+       setData(newData)
+       let Newset=new Set(newData.map((val)=>{return val.title}))
+       setTitles([...Newset])
+    }
+    fetchData()
+  },[])
+  
+ 
+  return(
+    <>
+    {
+  titles.map((val) => (
+    <button 
+      onClick={() => {
+        const selectedObject = data.find((newVal) => newVal.title === val);
+        setTitle(selectedObject || {}); // Ensure it's an object, not undefined
+        console.log(selectedObject); // Debugging: Check if the correct object is logged
+      }}  
+      style={{ padding: "10px", margin: "5px" }} 
+      key={val}
+    >
+      {val}
+    </button>
+  ))
+}
+
+   {
+    <h1>{showTitle.title}</h1>
+   }
+    
+    </>
+  )
 };
 export default App;
